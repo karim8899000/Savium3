1ada3c2216841e21f52d3a267f2a001893839bca
karim8899000 <167581656+karim8899000@users.noreply.github.com>
2024-05-05 12:41:53 +0300

    app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ ch='qwertyuioplkjhgfdsazxcvbnm'
 def create():
  global created
  global failed
- user=str(random.choice('qwertyuioplkjhgfdsazxcvbnm')[0])+str(''.join(random.choice(ch) for i in range(11)))
+ user=str(random.choice('qwertyuioplkjhgfdsazxcvbnm')[0])+str(''.join(random.choice(ch) for i in range(4)))
  
  #user='kdvdfejevfsheh'
  
