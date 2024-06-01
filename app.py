241ff83c93d56f61076e4cfbdf49c48df4d71174
karim8899000 <167581656+karim8899000@users.noreply.github.com>
2024-06-02 02:54:06 +0300

    Update app.py

--- app.py
+++ app.py
@@ -21,8 +21,8 @@ failed=0
 G = '\033[1;32m'
 L = '\033[1;31m'
 
-own_id= 6827776266
-tele_bot='6758820487:AAGVG7WEAHQn78fuOpVrEx4l1uIQLtwGj88'
+own_id= 761058551
+tele_bot='7154902977:AAHTiZJsaxVNVlSU46QQUWf8q3NTRp7Txds'
 ch='qwertyuioplkjhgfdsazxcvbnm'
 def create():
  global created
